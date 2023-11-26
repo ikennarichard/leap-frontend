9a76e2bf34f78a5f7ee4f001c8c3acced6587b88
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-26 18:24:46 +0100

    Add signin link for the onboarding screen

---
+++ src/components/SignInLink.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const SignInLink = () => {
+  return (
+    <div>
+      <small>Have an account?</small>
+      <span><Link>Login</Link></span>
+    </div>
+  )
+}
+
+export default SignInLink
