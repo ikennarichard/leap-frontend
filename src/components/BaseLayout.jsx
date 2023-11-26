5ff32f45de929f3004433847e70fac17585dcd47
Ikenna Richard <oguejioforrichard@gmail.com>
2023-11-26 18:21:02 +0100

    Add base layout

---
+++ src/components/BaseLayout.jsx
@@ -0,0 +1,12 @@
+// import { Outlet } from "react-router-dom";
+import LandingPage from "../pages/landing_page";
+
+const BaseLayout = () => {
+  return (
+    <>
+      <LandingPage/>
+    </>
+  )
+}
+
+export default BaseLayout;
