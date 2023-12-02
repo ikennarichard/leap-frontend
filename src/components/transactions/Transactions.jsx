f11c15c8649b4ae38472b666a0eef12f1021032f
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-02 14:28:17 +0100

    Refactor code

--- src/components/transactions/Transactions.jsx
+++ src/components/transactions/Transactions.jsx
@@ -1,44 +1,21 @@
-import { Link } from 'react-router-dom';
-import styles from './transactions.module.css'
-import Naira from '../currencies/Naira';
+import PageHeading from '../PageHeading.jsx/PageHeading';
+import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Transactions = () => {
 
-  function formatDate(date) {
-    return new Intl.DateTimeFormat('en-US', {
-      day: 'numeric',
-      month: 'short',
-      year: 'numeric'
-    }).format(date).replace(',', '').split(' ').join('-');
-  }
-
+  const { pathname } = useLocation();
+  
   return (
-    <div>
-      <ul className={styles.transactions}>
-        <div className={styles.transaction_heading}>
-          <h2>Recent transactions</h2>
-          <button className='reset_btn'>
-            <Link>
-              See all
-              <img src="arrow-right.svg" alt="" />
-            </Link>
-          </button>
-        </div>
-        <li>
-          <div className={styles.transaction_details}>
-            <img src="transactions/transfer.svg" alt="" />
-            <div>
-              <p>Adeola sent you<strong>
-                <Naira amount={1000}/>
-              </strong></p>
-              <small>Bank Transfer</small>
-              <span>.</span>
-              <small>{formatDate(new Date('2023-10-23'))}</small>
-            </div>
-          </div>
-          <hr />
-        </li>
-      </ul>
+    <div style={{padding: '2em 1em'}}>
+      <div>
+        <PageHeading 
+          heading='Transactions'
+          link={pathname === '/transactions' ? '/' : ''}
+        />
+      </div>
+
+      <Outlet/>
     </div>
   )
 }
