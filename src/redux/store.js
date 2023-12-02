0011f3b733040144e2c0fb3f26d6ddaac3bcbdbb
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-02 14:35:37 +0100

    Update redux store slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../redux/auth/authSlice'
+import authReducer from './auth/authSlice'
+import transactionsReducer from './transactions/transactionSlice'
+import accountReducer  from './account/accountSlice'
+import beneficiariesReducer  from './beneficiaries/beneficiarySlice'
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    transactions: transactionsReducer,
+    account: accountReducer,
+    beneficiaries: beneficiariesReducer
   },
 })
