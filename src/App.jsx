06e9992b251d40e010f2e61e529f7f56951de5ff
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-02 14:25:01 +0100

    Update page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import BaseLayout from "./components/BaseLayout";
-// import ProfilePage from "./pages/profile_page";
-import BeneficiaryPage from "./pages/beneficiaries/beneficiaries";
+import ProfilePage from "./pages/profile_page/profile_page";
+import Beneficiaries from "./pages/beneficiaries/beneficiaries";
 import Signup from "./pages/sign_up_page/signup";
 import Sign_In from "./pages/sign_in/sign_in";
-import Transfer from "./pages/transfer";
+import Accounts from "./pages/accounts/accounts";
+// import ExchangeRates from "./pages/exchange_rates/exchange";
+import Transfer from "./pages/transfer/transfer";
+import Transactions from "./components/transactions/Transactions";
+import TransactionsList from "./components/transactions/TransactionsList";
+import TransactionTracker from "./components/transactions/TransactionTracker";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 const router = createBrowserRouter([
@@ -14,18 +19,42 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: '',
+        element: <ProfilePage/>,
       },
+
+      {
+        path: 'transactions',
+        element: <Transactions />,
+        children: [
+          {
+            index: true,
+            element: <TransactionsList/>
+          },
+
+          {
+            path: ':id',
+            element: <TransactionTracker/>
+          }
+        ]
+      },
+
+      {
+        path: 'accounts',
+        element: <Accounts />
+      },
+
       {
         path: 'beneficiaries',
-        element: <BeneficiaryPage />,
+        element: <Beneficiaries />,
       },
+
       {
-        path: 'transfers',
-        element: <Transfer/>,
+        path: 'transfer',
+        element: <Transfer />,
       },
     ],
   },
+
   {
     path: 'sign_up',
     element: <Signup />
