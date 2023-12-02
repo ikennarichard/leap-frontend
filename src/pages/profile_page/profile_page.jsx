b565dbbc63380c9eb8ac2299752e1b06373331b6
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-02 14:32:41 +0100

    Add recent transactions list

--- src/pages/profile_page/profile_page.jsx
+++ src/pages/profile_page/profile_page.jsx
@@ -1,13 +1,25 @@
-import AccountBalance from '../components/Balance';
-import Transactions from '../components/Transactions';
-import styles from './profile.module.css'
+import AccountBalance from '../../components/account_balance/Balance';
+import styles from './profile.module.css';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Transaction from '../../components/transactions/Transaction';
+import { nanoid } from '@reduxjs/toolkit';
 
 const ProfilePage = () => {
+
+  const transactions = useSelector(state => state.transactions.transactions);
+
+    const sortedTransactions = Array.from(transactions).sort((a, b) => new Date(b.transactionDate) - new Date(a.transactionDate)).slice(0, 5);
+  
   return (
-    <section>
+    <section className={styles['profile_container']}>
       <div className={styles.heading}>
         <div className={styles.user}>
-          <img src="avatar.svg" alt="user image" />
+          <img 
+            src="avatar.svg" 
+            alt="user image"
+            className={styles['avatar']} 
+          />
           <h2>Hi Valerie,</h2>
         </div>
         <div className={styles.check_rates}>
@@ -16,13 +28,13 @@ const ProfilePage = () => {
         </div>
       </div>
       <AccountBalance/>
-      <div>
+      <div className={styles['more-action']}>
         <h3>More Actions</h3>
         <div className={styles.actions}>
-          <div>
+          <Link to='/accounts' className='reset-link'>
             <img src="more_actions/accounts.svg" alt="" />
-            <p>Accounts</p>
-          </div>
+            <span>Accounts</span>
+          </Link>
           <div>
             <img src="more_actions/pay_bills.svg" alt="" />
             <p>Pay Bills</p>
@@ -31,13 +43,29 @@ const ProfilePage = () => {
             <img src="more_actions/card.svg" alt="" />
             <p>Cards</p>
           </div>
-          <div>
+          <Link className={`reset-link`} to='/beneficiaries'>
             <img src="more_actions/beneficiaries.svg" alt="" />
-            <p>Beneficiaries</p>
-          </div>
+            <span>Beneficiaries</span>
+          </Link>
         </div>
       </div>
-      <Transactions/>
+
+      <ul className={styles['recent-transactions']}>
+        <div className={styles['recent-transactions-heading']}>
+          <h2>Recent transactions</h2>
+          <Link 
+            to='/transactions' 
+            className={`reset-link ${styles['see-all']}`}
+          >
+            <span>See all</span> <img src="arrow-right.svg" alt="" />
+          </Link>
+        </div>
+        {
+          sortedTransactions.map((t) => (
+            <Transaction t={t} key={nanoid()} path='transactions/'/>
+          ))
+        }
+      </ul>
     </section>
   )
 }
