efb98362de5678ba213c2cb43ee801bb255869cf
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-11 23:46:16 +0100

    Add exchange reducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import authReducer from './auth/authSlice'
 import transactionsReducer from './transactions/transactionSlice'
 import accountReducer  from './account/accountSlice'
 import beneficiariesReducer  from './beneficiaries/beneficiarySlice'
+import ratesReducer from './exchange_rates/exchangeRatesSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     transactions: transactionsReducer,
     account: accountReducer,
-    beneficiaries: beneficiariesReducer
+    beneficiaries: beneficiariesReducer,
+    rate: ratesReducer
   },
 })
