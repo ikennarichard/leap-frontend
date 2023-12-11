185c4aa08c4a201e5af0d4b8c8d710c45efaf5ed
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-11 23:44:39 +0100

    Update exchnage component

--- src/pages/exchange_rates/exchange.jsx
+++ src/pages/exchange_rates/exchange.jsx
@@ -1,49 +1,84 @@
 import styles from './exchange.module.css';
-import BackArrow from '../../components/BackArrow/BackArrow';
-import SelectCurrency from '../../components/select_currency/SelectCurrency';
+import { useState, useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import PageHeading from '../../components/PageHeading.jsx/PageHeading';
+import Select from 'react-select';
+import * as l from '../../components/utils/utils'
+import axios from 'axios';
 
 const ExchangeRates = () => {
+  const [baseCurrency, setBaseCurrency] = useState('NGN');
+  const [currency, setCurrency] = useState('naira');
+  const [exchangeRates, setExchangeRates] = useState({});
+  const rates = useSelector((state) => state.rate.currencies);
 
-  const handleSwitch = () => {
-    console.log('switch');
+  const dateRef = useRef();
+  
+
+  // map accounts of value and label
+  const rateOptions = Object.keys(rates).map((a) => ({
+    label:`${rates[a].country} - ${rates[a].currencyCode}`,
+    value: rates[a]
+    }))
+
+  const handleChange = (selected) => {
+    setBaseCurrency(selected.value.currencyCode);
+    setCurrency(selected.value.currency);
   }
+
+  useEffect(() => {
+    const fetchExchangeRates = async () => {
+      try {
+        const response = await axios.get(`https://v6.exchangerate-api.com/v6/bb77863289c1abaa0eaf3a88/latest/${baseCurrency}`);
+        if (response.status !== 200) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.data;
+        setExchangeRates(data.conversion_rates);
+        dateRef.current = data.time_last_update_utc
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+    };
+
+    fetchExchangeRates();
+  }, [baseCurrency]);
 
   return (
     <section className={styles.container}>
       <div className={styles.exchange_heading}>
-        <BackArrow link='/profile_page'/>
-        <h2>Exchange Rates</h2>
+        <PageHeading link='/' heading='Exchange Rates'/>
       </div>
 
       <p className={styles.last_update}>
-        Last updated at *dynamic date*
+        Last updated at {dateRef.current ? dateRef.current : 'Loading...'}
       </p>
 
-      <div className={styles.exchange_currency}>
-        <p>Exchange currency</p>
-        <SelectCurrency/>
-      </div>
+      <form>
+        <div className="field">
+          <Select 
+            options={rateOptions}
+            placeholder='Exchange currency'
+            onChange={handleChange}
+            isSearchable
+          />
+        </div>
+      </form>
 
-      <ul className={styles.exchange_list_container}>
-        <li className={styles.exchange_list_item}>
-          <div className={styles.exchange_conversion}>
-            <p>NGN to USD</p>
-            <small>N1 =</small>
-            <small>$0.9</small>
-          </div>
-
-          <button 
-            type='button' 
-            onClick={handleSwitch}
-            className={`${styles.switch_btn} reset_btn`}
-            >
-              <img 
-                src="switch.svg" 
-                className={styles.switch_icon} 
-              />
-              Switch
-          </button>
-        </li>
+      <ul className={styles.exchange_list}>
+        {
+          Object.keys(rates).filter(r => r !== baseCurrency).map((curr) => (
+            <li className={styles.exchange_list_item} key={curr.currencyCode}>
+              <div className={styles.exchange_conversion}>
+                <p>{baseCurrency} to {curr}</p>
+                <small>
+                  {l.getAmountByCurrencyType(currency, 1)}
+                </small>
+                <small>= {l.getCurrencySymbol(rates[curr].currencyCode)}{exchangeRates[curr]}</small>
+              </div>
+            </li>
+          ))
+        }
       </ul>
     </section>
   )
