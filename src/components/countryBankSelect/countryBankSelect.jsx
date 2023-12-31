e58d21637edecb7993e51fd22d04d7fb6136e495
Ikenna Richard <oguejioforrichard@gmail.com>
2023-12-31 16:31:30 +0100

    Refactor code

--- src/components/countryBankSelect/countryBankSelect.jsx
+++ src/components/countryBankSelect/countryBankSelect.jsx
@@ -5,40 +5,50 @@ import Select from 'react-select';
 
 const CountryBankSelector = ({ onUpdateUserDetails }) => {
 
-  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState('Nigeria');
   const [selectedBank, setSelectedBank] = useState('');
 
   const { banks, countries } = useSelector(state => state.auth);
 
-  const handleCountryChange = selectedOption => {
-    setSelectedCountry(selectedOption);
-    setSelectedBank(null);
-    onUpdateUserDetails({ country: selectedOption.label, bankName: null });
-    
-  };
+  const countryOptions = countries.map((c) => ({
+    value: c,
+    label: `${c.name} - ${c.currency.toUpperCase()}`
+  }));
 
-  const handleBankChange = selectedOption => {
-    setSelectedBank(selectedOption);
-    onUpdateUserDetails({ country: selectedCountry.label, bankName: selectedOption.label });
-  };
+  const bankOptions = banks[selectedCountry].map((bank) => ({
+    label:`${bank.name} - ${selectedCountry}`,
+    value: bank.name
+  }))
+
+  const handleSelectChange = (selected, {name}) => {
+    if (name === 'country name') {
+     setSelectedCountry(selected.value.name);
+     onUpdateUserDetails({ country: selectedCountry, bankName: null });
+     console.log(selectedCountry)
+    } else if (name === 'bank name') {
+     setSelectedBank(selected.value)
+     onUpdateUserDetails({ country: selectedCountry, bankName: selectedBank })
+     console.log(selectedBank)
+    } 
+   }
 
   return (
     <>
     <div className='field'>
       <label>Select a Country</label>
       <Select
-        value={selectedCountry}
-        onChange={handleCountryChange}
-        options={countries}
+        name='country name'
+        onChange={handleSelectChange}
+        options={countryOptions}
       />
     </div>
 
     <div className='field'>
       <label>Select a Bank</label>
       <Select
-        value={selectedBank}
-        onChange={handleBankChange}
-        options={banks[selectedCountry.value]}
+        name='bank name'
+        onChange={handleSelectChange}
+        options={bankOptions}
       />
     </div>
     </>
